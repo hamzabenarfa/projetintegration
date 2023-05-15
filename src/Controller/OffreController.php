9d7ebadfff1e373dda4683e14e4d65d45e0c60f9
Your Name <hamzabenarfa4@gmail.com>
2023-05-15 11:04:59 +0100

    added user controller

--- src/Controller/OffreController.php
+++ src/Controller/OffreController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/offre')]
-class sOffreController extends AbstractController
+class OffreController extends AbstractController
 {
     #[Route('/', name: 'app_offre_index', methods: ['GET'])]
     public function index(OffreRepository $offreRepository): Response
