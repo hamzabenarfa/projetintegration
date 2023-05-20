24447e8c752673c8400e256bce34a9c405316b8f
ahmed <abdeliahmed204@gmail.com>
2023-05-20 16:46:46 +0100

    message

--- migrations/Version20230520142759.php
+++ migrations/Version20230520142759.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230518191408 extends AbstractMigration
+final class Version20230520142759 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20230518191408 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE voyage ADD prix DOUBLE PRECISION NOT NULL, ADD img_src VARCHAR(255) DEFAULT NULL, ADD dest_title VARCHAR(255) DEFAULT NULL, ADD location VARCHAR(255) DEFAULT NULL, ADD description VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE voyage ADD img_src VARCHAR(255) DEFAULT NULL, ADD dest_title VARCHAR(255) DEFAULT NULL, ADD location VARCHAR(255) DEFAULT NULL, ADD description VARCHAR(255) DEFAULT NULL, DROP destination');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE voyage DROP prix, DROP img_src, DROP dest_title, DROP location, DROP description');
+        $this->addSql('ALTER TABLE voyage ADD destination VARCHAR(255) NOT NULL, DROP img_src, DROP dest_title, DROP location, DROP description');
     }
 }
